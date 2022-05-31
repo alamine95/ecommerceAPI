178875743400cbbaddaec3fe26a9bdd9b3340ad9
alamine95 <lsow313@gmail.com>
2022-05-31 22:26:10 +0000

    stripe

--- routes/order.js
+++ routes/order.js
@@ -70,26 +70,28 @@ router.get("/income", verifyTokenAndAdmin, async (req, res) => {
     const date = new Date();
     const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
     const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
-
+  
     try {
-        const income = await Order.aggregate([
-            { $match: { createdAt: { $gte: previousMonth}}},
-            {
-                $project: {
-                    month: { $month: "$createdAt"},
-                    sales: "$amount",
-                },
-                $group:{
-                    _id:"$month",
-                    total:{$sum: "$sales"}
-                },
-            },
-        ]);
-        res.status(200).json(income);
+      const income = await Order.aggregate([
+        { $match: { createdAt: { $gte: previousMonth } } },
+        {
+          $project: {
+            month: { $month: "$createdAt" },
+            sales: "$amount",
+          },
+        },
+        {
+          $group: {
+            _id: "$month",
+            total: { $sum: "$sales" },
+          },
+        },
+      ]);
+      res.status(200).json(income);
     } catch (err) {
-        res.status(500).json(err);
+      res.status(500).json(err);
     }
-})
+  });  
 
 
 
